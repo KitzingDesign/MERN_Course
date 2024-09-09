438891c7fb30365cba655826aabe7aed1002dca9
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2024-09-09 14:07:40 +0200

    everything to start lecture 2

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://www.dandyrepaishop.com",
+  "https://dandyrepaishop.com",
+];
+
+module.exports = allowedOrigins;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route for serving 'index.html'
+app.use("/", require("./routes/root"));
+
+//for 404
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+app.use(errorHandler);
+
+// Start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
